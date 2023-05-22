1faaa4cb3f91b01e50faa3c114906c7580cbee1c
Baptiste Legat <legatbaptiste@gmail.com>
2023-05-22 15:59:24 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header></Header>
+    <!-- Le reste du contenu de votre application -->
+  </div>
+</template>
+
+<style scoped>
+main {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+</style>
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<!--  header.vue -->
+<template>
+    <header>
+      <h1>Mon en-tête</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">À propos</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+<style scoped>
+
+header {
+    background-color: #f2f2f2;
+    padding: 10px;
+}
+
+h1 {
+    font-size: 2em;
+    text-align: center;
+    color: #333;
+}
+
+nav {
+    text-align: center;
+}
+
+nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+nav ul li a {
+    font-size: 1.2em;
+    color: #333;
+    text-decoration: none;
+}
+
+</style>
+<script>
+export default {
+    name: 'Header',
+}
+</script>
