304cf389626f458193532e7b344b351997cccd9e
ilyes <ilyes.deochandiano@ynov.com>
2023-05-23 11:44:12 +0200

    Innstauration de la carte

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,14 @@
     <Header></Header>
     <!-- Le reste du contenu de votre application -->
   </div>
+  <div>
+      <card></card>
+  </div>
 </template>
 
+
 <style scoped>
+
 main {
   margin: 0 auto;
   max-width: 1200px;
@@ -13,9 +18,11 @@ main {
 </style>
 <script>
 import Header from './components/Header.vue'
+import Card from "@/components/Card.vue";
 
 export default {
   components: {
+      Card,
     Header,
   }
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,52 @@
 <!--  header.vue -->
 <template>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cosmo/bootstrap.min.css" integrity="sha384-5QFXyVb+lrCzdN228VS3HmzpiE7ZVwLQtkt+0d9W43LQMzz4HBnnqvVxKg6O+04d" crossorigin="anonymous">
+
     <header>
-      <h1>Mon en-tête</h1>
-      <nav>
-        <ul>
-          <li><a href="#">Accueil</a></li>
-          <li><a href="#">À propos</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor03">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home
+                                <span class="visually-hidden">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Separated link</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-sm-2" type="search" placeholder="Search">
+                        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
     </header>
   </template>
 <style scoped>
+
 
 header {
     background-color: #f2f2f2;
@@ -20,8 +55,10 @@ header {
 
 h1 {
     font-size: 2em;
-    text-align: center;
     color: #333;
+    margin-top:10px;
+
+
 }
 
 nav {
@@ -45,9 +82,11 @@ nav ul li a {
     text-decoration: none;
 }
 
+
 </style>
 <script>
 export default {
     name: 'Header',
 }
+
 </script>
