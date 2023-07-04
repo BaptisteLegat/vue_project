b569d8d696b6718157d7ef806ba7772bdec75bcf
ilyes <ilyes.deochandiano@ynov.com>
2023-07-04 12:06:39 +0200

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,106 @@
+<template>
+    <v-sheet class="pa-12" rounded>
+        <v-card class="mx-auto mt-15 px-6 py-8" max-width="344">
+            <v-form
+                v-if="!loggedIn"
+                v-model="form"
+                @submit.prevent="onSubmit"
+            >
+                <v-text-field
+                    v-model="email"
+                    :readonly="loading"
+                    :rules="[required]"
+                    class="mb-2"
+                    clearable
+                    label="Email"
+                ></v-text-field>
+                <v-text-field
+                    v-model="password"
+                    :readonly="loading"
+                    :rules="[required]"
+                    clearable
+                    label="Password"
+                    placeholder="Enter your password"
+                ></v-text-field>
+                <br />
+                <v-btn
+
+                    :disabled="!form"
+                    :loading="loading"
+                    block
+                    color="indigo"
+                    size="large"
+                    type="submit"
+                    variant="elevated"
+                >
+                    Connexion
+                </v-btn>
+            </v-form>
+            <router-view v-else></router-view>
+        </v-card>
+    </v-sheet>
+</template>
+
+<script>
+import { setLoggedIn } from "@/router";
+
+export default {
+    name: "Login",
+
+    data() {
+        return {
+            form: false,
+            email: null,
+            password: null,
+            loading: false,
+            loggedIn: false,
+        };
+    },
+
+    methods: {
+        onSubmit() {
+            if (!this.form) return;
+            this.loading = true;
+
+            // Appel à l'API pour se connecter
+            fetch("https://sf-clothes-api.ld-web.net/api/login_check", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: this.email,
+                    password: this.password,
+                }),
+            })
+                .then((response) => {
+                    // Traiter la réponse de l'API ici
+                    if (response.ok) {
+                        // Connexion réussie
+                        console.log("Login successful");
+                        setLoggedIn(true);
+                        this.$router.push("/"); // Rediriger vers la page "Home"
+                    } else {
+                        // Connexion échouée
+                        console.log("Login failed");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+        required(v) {
+            return !!v || "Field is required";
+        },
+    },
+};
+</script>
+
+<style>
+.mt-8 {
+    margin-top: 1rem; /* Ajustez la valeur pour déplacer le formulaire plus ou moins bas */
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,44 @@
-    import { createRouter, createWebHistory } from "vue-router";
-    import DetailProduct from "@/components/DetailProduct.vue";
-    const routes = [
-        {
-            path: "/",
-            name: "Home",
-            component: () => import("../components/Home.vue")
-        },
-        {
-            path: "/detailProduct/:id",
-            name: "DetailProduct",
-            component: DetailProduct
-        }
-    ];
+import { createRouter, createWebHistory } from "vue-router";
+import DetailProduct from "@/components/DetailProduct.vue";
+import Login from "@/components/Login.vue";
 
-    const router = createRouter({
-        history: createWebHistory("/"),
-        routes
-    });
+let loggedIn = false; // Variable loggedIn
 
-    export default router;
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("../components/Home.vue")
+    },
+    {
+        path: "/detailProduct/:id",
+        name: "DetailProduct",
+        component: DetailProduct
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory("/"),
+    routes
+});
+
+// Fonction pour définir la valeur de loggedIn
+export function setLoggedIn(value) {
+    loggedIn = value;
+}
+
+// Ajout de la redirection vers la page "Login" par défaut
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'Login' && !loggedIn) {
+        next({ name: 'Login' });
+    } else {
+        next();
+    }
+});
+
+export default router;
